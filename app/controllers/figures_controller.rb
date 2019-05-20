68c4e0de390d87d257daf91f1ef426cdd91fee0c
Dan Hintz <dhintz@live.com>
2019-05-20 02:07:59 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,28 @@
 class FiguresController < ApplicationController
   # add controller methods
+  
+  get '/figures' do 
+    @figures = Figure.all
+    erb :"figures/index"
+  end
+
+  post "/figures" do
+    @figure = Figure.create(params[:figure])
+    # params[:figure][:title_ids].each {|title_id| @figure.titles << Title.find_or_create_by(id: title_id)}
+    # params[:figure][:landmark_ids].each {|landmark_id| @figure.landmarks << Landmark.find_or_create_by(id: landmark_id)}
+    # @figure.save
+    redirect "/figures/#{@figure.id}"
+  end
+  
+  get "/figures/new" do
+    @landmarks = Landmark.all
+    @titles = Title.all
+    erb :"figures/new"
+  end
+  
+  get "/figures/:id" do
+    @figure = Figure.find_by_id(params[:id])
+    erb :"figures/show"
+  end
+  
 end
